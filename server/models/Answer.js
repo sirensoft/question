b14995e216069a748bb51059828b3380114ff277
tehnn <tehnnn@gmail.com>
2018-02-11 18:33:21 +0700

    Sun 02/11/2018 18:33:21.51

--- server/models/Answer.js
+++ server/models/Answer.js
@@ -1,14 +1,19 @@
 var knex = require('../config/knex');
 class Answer{
     async add(jdata){        
-        try {         
-            let res = await knex('answer').insert({topic_id:7,rawdata:JSON.stringify(jdata)});          
+        try {  
+
+           
+            jdata = JSON.stringify(jdata);     
+            let res =  knex('answer').insert({topic_id:7,rawdata:jdata});  
+            
+            
+
             return res;
         } catch (error) {
             return error;
-        }        
-       
-        
+        }      
+          
     }
 }
 module.exports = new Answer();
