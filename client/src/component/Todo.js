f292641e5a290f1cbb5bd1905f14f77d144dceee
tehnn <tehnnn@gmail.com>
2018-02-13 14:50:56 +0700

    Tue 02/13/2018 14:50:55.87

--- client/src/component/Todo.js
+++ client/src/component/Todo.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 
 import Form from "react-jsonschema-form";
-import SweetAlert from 'sweetalert2-react';
+
 
  class Todo extends Component{
 
@@ -9,60 +9,68 @@ import SweetAlert from 'sweetalert2-react';
         super(props)
         console.log('contruct','....contruct')
         this.state= {         
-            schema :null 
-            
-        }
-        
+            schema :null,
+            formData:null ,
+             
+        }        
     }
+    componentWillMount(){      
+        this.getSchema(3); 
+     }    
     
     getSchema=async(id)=>{
         let raw = await fetch('/topic/'+id)
         let schema =await raw.json()       
         this.setState({
             schema:schema
-        })
-        
-    }
+        })        
+    } 
 
-  
-
-    componentWillMount(){      
-       this.getSchema(3); 
-    }
    
-    onSubmit=({schema,formData})=>(        
-          this.addAnswer({id:schema.id,answer:formData})        
-    );
+    onSubmit=async ({schema,formData})=>{     
+        
+        let res = await this.addAnswer({id:schema.id,answer:formData}) ;
+        console.log('submit',res);
+        if(res===1){            
+            alert("สำเร็จ");
+            this.setState({
+                formData:null
+            });
+        }
+
+    };
 
     addAnswer = async (data)=>{
-        let res = await fetch('/answer/create', {
+        let resp = await fetch('/answer/create', {
             method: 'POST',           
             body: JSON.stringify(data),
             headers: {
                 'Content-Type': 'application/json'
             }
         })
-        return res;  
+        let res = await resp.json();
+        return res.rowCount;  
     }   
      
     render(){        
         const log = (type) => console.log.bind(console, type);         
-        
         return(
             <div>
              {(this.state.schema)?
-                <Form schema={this.state.schema}
+                <Form 
+                    schema={this.state.schema}
+                    formData={this.state.formData}
                     onChange={log("changed")}
                     onSubmit={this.onSubmit}
                     onError={log("errors")} >
                 </Form>
                 :'Loading...'
-             }                     
-           
+             }    
+             
+            
             </div>
         )
     }
-
 }
 
 export default Todo;
